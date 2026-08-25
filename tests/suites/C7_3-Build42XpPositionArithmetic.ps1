feeec8c3804ac0584e538b2f2c834fdc66ae2770
Skeptic043 <139933279+Skeptic043@users.noreply.github.com>
2026-08-25 09:21:55 -0700

    Add Build 42 XP position arithmetic

---
+++ tests/suites/C7_3-Build42XpPositionArithmetic.ps1
@@ -0,0 +1,10 @@
+[pscustomobject]@{
+    Label = 'C7.3 Build 42 XP position arithmetic'
+    Spec = 'tests/adapters/Build42XpPositionArithmeticSpec.lua'
+    Sources = @(
+        [pscustomobject]@{
+            Global = 'Build42XpPositionArithmetic'
+            Path = 'Contents/mods/SurvivorLevelingAdvancement/42.20/media/lua/shared/SurvivorLevelingAdvancement/Adapters/Build42XpPositionArithmetic.lua'
+        }
+    )
+}
