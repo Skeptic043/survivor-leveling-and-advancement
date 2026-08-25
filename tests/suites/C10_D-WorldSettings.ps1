c38448b979fd5aa22f0da246c06962fab6695217
Skeptic043 <139933279+Skeptic043@users.noreply.github.com>
2026-08-25 15:52:34 -0700

    Add runtime world settings resolver

---
+++ tests/suites/C10_D-WorldSettings.ps1
@@ -0,0 +1,7 @@
+[pscustomobject]@{
+    Label = 'C10-D WorldSettings'
+    Spec = 'tests/runtime/WorldSettingsSpec.lua'
+    Sources = @(
+        [pscustomobject]@{ Global = 'WorldSettings'; Path = 'Contents/mods/SurvivorLevelingAdvancement/42.20/media/lua/shared/SurvivorLevelingAdvancement/Runtime/WorldSettings.lua' }
+    )
+}
