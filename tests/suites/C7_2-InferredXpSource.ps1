4374468881189a9095233f71e7de28c56a3d2cb9
Skeptic043 <139933279+Skeptic043@users.noreply.github.com>
2026-08-25 08:39:50 -0700

    Add inferred XP source

---
+++ tests/suites/C7_2-InferredXpSource.ps1
@@ -0,0 +1,10 @@
+[pscustomobject]@{
+    Label = 'C7.2 Inferred XP source'
+    Spec = 'tests/xp/InferredXpSourceSpec.lua'
+    Sources = @(
+        [pscustomobject]@{
+            Global = 'InferredXpSource'
+            Path = 'Contents/mods/SurvivorLevelingAdvancement/42.20/media/lua/shared/SurvivorLevelingAdvancement/XP/InferredXpSource.lua'
+        }
+    )
+}
