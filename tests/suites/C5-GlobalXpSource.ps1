133fd6b50cb7a0b83ddb88552ffd55d57b364076
Skeptic043 <139933279+Skeptic043@users.noreply.github.com>
2026-08-24 21:10:46 -0700

    Implement exact global XP source

---
+++ tests/suites/C5-GlobalXpSource.ps1
@@ -0,0 +1,10 @@
+[pscustomobject]@{
+    Label = 'C5 Global XP Source'
+    Spec = 'tests/xp/GlobalXpSourceSpec.lua'
+    Sources = @(
+        [pscustomobject]@{
+            Global = 'GlobalXpSource'
+            Path = 'Contents/mods/SurvivorLevelingAdvancement/42.20/media/lua/shared/SurvivorLevelingAdvancement/XP/GlobalXpSource.lua'
+        }
+    )
+}
